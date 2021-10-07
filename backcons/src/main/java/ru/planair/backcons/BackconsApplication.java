2130a8024218db9a62b26cb85103b3f2b0360df0
Васильцов Виталий Олегович <VOVasiltsov@sberbank.ru>
2021-10-07 21:50:02 +0300

    init

---
+++ backcons/src/main/java/ru/planair/backcons/BackconsApplication.java
@@ -0,0 +1,13 @@
+package ru.planair.backcons;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BackconsApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BackconsApplication.class, args);
+    }
+
+}
